a316dc4ae73bd57e94e333fee156983ef1c9a75c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-12-30 09:54:41 -0600

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -15,60 +15,57 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter BottomSheet Demo'),
+      home: MyHomePage(title: 'BottomSheet Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
+  final sheetWidget = Text(
+    'I am in a BottomSheet.',
+    style: TextStyle(color: Colors.white, fontSize: 24),
+  );
+
   final String title;
 
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var sheetWidget = Text(
-    'I am in a BottomSheet.',
-    style: TextStyle(color: Colors.white, fontSize: 24),
-  );
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         // Using Builder just to get a Context inside Scaffold
         // so the showBottomSheet function can find the Scaffold.
         child: Builder(
-          builder: (context) => Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                child: Text('Show Non-Modal BottomSheet'),
-                // openBottomSheet is defined in bottom_sheet_button.dart.
-                onPressed: () => openBottomSheet(
-                  backgroundColor: Colors.green,
-                  context: context,
-                  includeCloseButton: true,
-                  modal: false,
-                  widget: sheetWidget,
-                ),
+          builder: (context) {
+            var nonModalButton = ElevatedButton(
+              child: Text('Show Non-Modal BottomSheet'),
+              onPressed: () => openBottomSheet(
+                backgroundColor: Colors.green,
+                context: context,
+                includeCloseButton: true,
+                modal: false,
+                widget: sheetWidget,
               ),
-              ElevatedButton(
-                child: Text('Show Modal BottomSheet'),
-                onPressed: () => openBottomSheet(
-                  context: context,
-                  modal: true,
-                  widget: sheetWidget,
-                ),
+            );
+
+            var modalButton = ElevatedButton(
+              child: Text('Show Modal BottomSheet'),
+              onPressed: () => openBottomSheet(
+                context: context,
+                modal: true,
+                widget: sheetWidget,
               ),
-            ],
-          ),
+            );
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[nonModalButton, modalButton],
+            );
+          },
         ),
       ),
     );
