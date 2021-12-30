56cb966d69e9b362ed14f90cecac7e307d6340ad
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-12-30 07:31:40 -0600

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bottom_sheet_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,40 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Builder(
-              builder: (context) => ElevatedButton(
-                child: Text('Show BottomSheet'),
-                onPressed: () {
-                  showBottomSheet<void>(
-                    context: context,
-                    builder: (context) => SafeArea(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          child: Column(children: [
-                            Text(
-                              'I am in a BottomSheet.',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 24,
-                              ),
-                            ),
-                            ElevatedButton(
-                                child: Text('Close'),
-                                onPressed: () => Navigator.pop(context))
-                          ]),
-                          decoration: BoxDecoration(color: Colors.green),
-                          height: 100,
-                          width: double.infinity,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
+          children: <Widget>[BottomSheetButton()],
         ),
       ),
     );
